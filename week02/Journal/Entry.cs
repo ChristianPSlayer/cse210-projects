0b5670e42779aadd78609a587720ca341c3ae069
ChristianPSlayer <chiki0930@gmail.com>
2026-01-23 22:56:22 -0500

    jurnal

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Entry
+{
+    public string _date;
+    public string _prompText;
+    public string _entryText;
+
+    public void Display()
+    {
+        Console.WriteLine($"");
+    }
+}
+
