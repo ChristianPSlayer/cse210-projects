2893ad1e6a94ec712d3c9370592756e361f820b2
ChristianPSlayer <chiki0930@gmail.com>
2026-02-17 08:29:30 -0500

    Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+       List<Shape> shapes = new List<Shape>();
+
+       shapes.Add(new Square("green", 6));
+       shapes.Add(new Rectangle("blue", 6, 6));
+       shapes.Add(new Circle("red", 9));
+
+       foreach (Shape shape in shapes)
+        {
+         
+           Console.WriteLine($"The area of the {shape.GetColor()} {shape.GetType().Name} is {shape.GetArea():F2}");
+        }
     }
 }
